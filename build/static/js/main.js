651cc7e7c07b61ff1b04861ec1d7de54429c76c7
jellySWATy <111crafter111@gmail.com>
2020-01-22 10:36:33 +0300

    added development directory, added pop-up

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    new WOW().init();
+    new WOW({
+        mobile: false
+    }).init();
     svg4everybody({});
     let scrollItems = [
         document.querySelector('.product-info__more'),
@@ -7,29 +9,27 @@ document.addEventListener('DOMContentLoaded', () => {
         ...document.querySelectorAll('.header-nav__link')
     ],
         functionsTabs = document.querySelectorAll('.functions-tab'),
-        priceTabs = document.querySelectorAll('.price-tab'),
         inputForms = document.querySelectorAll('.form__input'),
         textArea = document.querySelector('.contacts-form__textarea'),
-        popUp = document.querySelector('.product-form'),
+        popUp = document.querySelector('.formPopUp'),
         popUpClose = document.querySelector('.pop-up-close'),
         popUpButtons = document.querySelectorAll('.open-popup'),
         wrapper = document.querySelector('.wrapper'),
-        forms = document.querySelectorAll('.form');
+        forms = document.querySelectorAll('.form'),
+        statusMessages = document.querySelectorAll('.message-status')
+        reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
     popUpButtons.forEach(item => item.addEventListener('click', () => {
-        popUp.classList.toggle('pop-up-active');
-        wrapper.classList.toggle('pop-up-active');
+        togglePopUp();
     }));
 
     popUpClose.addEventListener('click', () => {
-        popUp.classList.remove('pop-up-active');
-        wrapper.classList.remove('pop-up-active');
+        togglePopUp();
     });
 
     window.addEventListener('click', event => {
         if (popUp.classList.contains('pop-up-active') && event.target == wrapper) {
-            popUp.classList.remove('pop-up-active');
-            wrapper.classList.remove('pop-up-active');
+            togglePopUp();
         }
     });
 
@@ -44,78 +44,121 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector(`.${tabName}-tab`).classList.toggle('active');
     }));
 
-    priceTabs.forEach(tab => tab.addEventListener('click', () => {
-        const childNodes = Array.from(tab.parentNode.children)
-        childNodes.forEach(elem => elem.classList.remove('active'));
-        tab.classList.add('active');
-    }));
-
     inputForms.forEach(input => {
         input.addEventListener('focusin', () => {
-            input.classList.add('active');
-            input.previousElementSibling.classList.add('active');
+            focusInput(input);
         });
         input.addEventListener('focusout', () => {
             if (input.value == false) {
-                input.classList.remove('active');
-                input.previousElementSibling.classList.remove('active');
+                unfocusInput(input);
             }
         })
     });
 
     textArea.addEventListener('focusin', () => {
-        textArea.classList.add('active');
+        focusInput(textArea, true);
     });
 
     textArea.addEventListener('focusout', () => {
         if (textArea.value == false) {
-            textArea.classList.remove('active');
+            unfocusInput(textArea, true);
         }
     });
 
     forms.forEach(item => item.addEventListener('submit', function (e) {
         e.preventDefault();
-        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-        var formValue = item.querySelector('.email').value;
+        let inputs = item.querySelectorAll('.form__input'),
+            textarea = item.querySelector('.contacts-form__textarea'),
+            formValue = item.querySelector('.email').value;
         if (reg.test(formValue) == false) {
-            alert('Введите корректный e-mail');
+            callMessage(false, true);
             return false;
         } else {
+            inputs.forEach(input => {
+                unfocusInput(input);
+            });
+            if (textarea) {
+                unfocusInput(textarea, true)
+            }
+            togglePopUp();
+            
             let formData = new FormData(this);
             formData = Object.fromEntries(formData);
-            console.log(formData)
             ajaxSend(formData);
             this.reset();
         }
     }));
 
+    const focusInput = (input, textarea = false) => {
+        input.classList.add('active');
+        if (!textarea) {
+            input.previousElementSibling.classList.add('active');
+        }
+    }
+
+    const unfocusInput = (input, textarea = false) => {
+        input.classList.remove('active');
+        if (!textarea) {
+            input.previousElementSibling.classList.remove('active');
+        }
+    }
+
+    const togglePopUp = () => {
+        popUp.classList.toggle('pop-up-active');
+        wrapper.classList.toggle('pop-up-active');
+    }
+
+    const callMessage = (succsessStatus, valid = false) => {
+        let message;
+        if (valid) {
+            message = Array.from(statusMessages).filter(message => message.classList.contains('message-status--unvalid'))[0];
+            showMessage(message);
+        } else if (succsessStatus){
+            message = Array.from(statusMessages).filter(message => message.classList.contains('message-status--succsess'))[0];
+            showMessage(message);
+        } else {
+            message = Array.from(statusMessages).filter(message => message.classList.contains('message-status--error'))[0];
+            showMessage(message);
+        }
+    };
+    
+    const delay = ms => {
+        return new Promise(r => setTimeout(() => r(), ms));
+    };
+    
+    const showMessage = async message => {
+        message.classList.add('active');
+        await delay(4000);
+        message.classList.remove('active');
+    };
+    
+    const ajaxSend = formData => {
+        fetch("http://localhost:8081/api/v1/landing/send", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(formData)
+        })
+          .then(response => callMessage(true))
+          .catch(error => callMessage(false));
+    };
+    
+    const animation = duration => {
+        let temp;
+        return sel => {
+            cancelAnimationFrame(temp);
+            let start = performance.now();
+            let from = window.pageYOffset || document.documentElement.scrollTop,
+                to = document.querySelector(sel).getBoundingClientRect().top - 69;
+            requestAnimationFrame(function step(timestamp) {
+                let progress = (timestamp - start) / duration;
+                1 <= progress && (progress = 1);
+                window.scrollTo(0, from + to * progress | 0);
+                1 > progress && (temp = requestAnimationFrame(step))
+            })
+        }
+    };
     
 })
 
-const ajaxSend = (formData) => {
-    fetch("http://localhost:8081/api/v1/landing/send", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(formData)
-    })
-      .then(response => alert("Сообщение отправлено"))
-      .catch(error => console.error(error));
-};
-
-function animation(duration) {
-    var temp;
-    return function (sel) {
-        cancelAnimationFrame(temp);
-        var start = performance.now();
-        var from = window.pageYOffset || document.documentElement.scrollTop,
-            to = document.querySelector(sel).getBoundingClientRect().top - 69;
-        requestAnimationFrame(function step(timestamp) {
-            var progress = (timestamp - start) / duration;
-            1 <= progress && (progress = 1);
-            window.scrollTo(0, from + to * progress | 0);
-            1 > progress && (temp = requestAnimationFrame(step))
-        })
-    }
-}
